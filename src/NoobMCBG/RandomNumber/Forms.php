ba8aecfd9c29e14d31a4269653551082a773c8a4
Noob MCBG <84442886+NoobMCBG@users.noreply.github.com>
2021-12-30 10:25:39 +0700

    Update Forms.php

--- src/NoobMCBG/RandomNumber/Forms.php
+++ src/NoobMCBG/RandomNumber/Forms.php
@@ -22,7 +22,7 @@ declare(strict_types=1);
 
 namespace NoobMCBG\RandomNumber;
 
-use pocketmine\player\Player;
+use pocketmine\Player;
 use NoobMCBG\RandomNumber\RandomNumber;
 use NoobMCBG\RandomNumber\libs\jojoe77777\FormAPI\CustomForm;
 
